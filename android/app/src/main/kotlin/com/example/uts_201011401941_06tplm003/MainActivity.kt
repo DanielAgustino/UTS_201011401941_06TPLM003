4e3337db8f0be8350e67c5ad77108a1bd9f571b3
DanielAgustino <daneljr16@gmail.com>
2023-05-13 20:32:40 +0700

    UTS_Mobile Programing

---
+++ android/app/src/main/kotlin/com/example/uts_201011401941_06tplm003/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uts_201011401941_06tplm003
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daniel Agustino',
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          body: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 4.0,
+                  ),
+                  borderRadius: BorderRadius.zero),
+              child: Column(children: [
+                AppBar(
+                  title: Text("Shoes", style: TextStyle(color: Colors.black)),
+                  actions: [Image(image: NetworkImage("https://tse4.mm.bing.net/th?id=OIP.LMl92_aYCyzQhCI0U4jLcAAAAA&pid=Api&P=0"))],
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color.fromARGB(255, 171, 172, 178),
+                  child: Container(
+                    child: ListTile(
+                      title: Text('Nike SB Zoom Blazer Mid Premium'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            '',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 8,795',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("https://pngfolio.com/images/all_img/copy/1663547438Sneaker%20Clipart%20%20(10).png")),
+                    ),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color.fromARGB(69, 161, 198, 118),
+                  child: Container(
+                    child: ListTile(
+                      title: Text('Nike Air Zoom Pegasus'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens road running shoes',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 9,995',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("https://pngfolio.com/images/all_img/copy/1663547441Sneaker%20Clipart%20%20(54).png")),
+                    ),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color.fromARGB(255, 127, 165, 83),
+                  child: Container(
+                    child: ListTile(
+                      title: Text('Nike Zoom X Vapor Fly'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens road racing shoes',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 19,695',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("https://pngfolio.com/images/all_img/copy/1663547442Sneaker%20Clipart%20%20(26).png")),
+                    ),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color.fromARGB(255, 217, 138, 138),
+                  child: Container(
+                    child: ListTile(
+                      title: Text('Nike Air Force 1 S50'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Older kids shoe',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 6,295',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("https://pngfolio.com/images/all_img/copy/1663547443Sneaker%20Clipart%20%20(46).png")),
+                    ),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.amber,
+                  child: Container(
+                    child: ListTile(
+                      title: Text('Nike Waffle One'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens Shoe',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 8,295',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("https://pngfolio.com/images/all_img/copy/1663547485Sneaker%20Clipart%20%20(68).png")),
+                    ),
+                  ),
+                )
+              ]))),
+    );
+  }
+}
